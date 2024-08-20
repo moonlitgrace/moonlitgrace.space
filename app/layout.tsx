e8a670e38ae3cf0350525837732fb3246cf5e2c4
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-20 08:19:17 +0530

    feat: changes with shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
-  title: "moonlitspace",
-  description: "a graceful space",
+  title: "moonlitspace | Home",
+  description: "<description>",
 };
 
 export default function RootLayout({
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+        "dark min-h-screen font-sans antialiased",
+        fontSans.variable
+      )}>{children}</body>
     </html>
   );
 }
