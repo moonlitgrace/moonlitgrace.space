5399f8536c4e3e4c151256a5b9b60ec64dd7a0b3
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-20 22:53:16 +0530

    feat: change font and add tooltips

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Aleo as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 // Styles
