932a3c1347c989780e83693d1408b6d56a4cc388
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-20 22:15:34 +0530

    feat: complete navbar and tooltips

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,6 +14,11 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import { IconProps } from "@/interfaces/icon";
 import PencilIcon from "./icons/pencil";
+import PhotoIcon from "./icons/photo";
+import GithubIcon from "./icons/github";
+import MailIcon from "./icons/mail";
+import { Separator } from "./ui/separator";
+import LightIcon from "./icons/light";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -24,6 +29,12 @@ const Navbar = () => {
         href: string;
         icon: ({ variant, ...props }: IconProps) => React.JSX.Element;
         label: string;
+      }
+    },
+    socials: {
+      [key: string]: {
+        link: string;
+        icon: ({ variant, ...props }: IconProps) => React.JSX.Element;
       }
     }
   } = {
@@ -37,13 +48,28 @@ const Navbar = () => {
         href: '/writings',
         icon: PencilIcon,
         label: 'Writings'
+      },
+      photos: {
+        href: '/photos',
+        icon: PhotoIcon,
+        label: 'Photo'
+      }
+    },
+    socials: {
+      github: {
+        link: 'https://github.com/moonlitgrace',
+        icon: GithubIcon
+      },
+      mail: {
+        link: 'mailto:moonlitgrace.gaia@gmail.com',
+        icon: MailIcon
       }
     }
   }
 
   return (
-    <div className="fixed left-1/2 transform -translate-x-1/2 bottom-10 flex items-center gap-2 border rounded-2xl p-2">
-      <TooltipProvider delayDuration={500}>
+    <TooltipProvider delayDuration={500}>
+      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-10 flex items-center gap-2 border rounded-2xl p-2">
         {Object.values(MAPPING.links).map((item, idx) => (
           <Tooltip key={idx}>
             <TooltipTrigger asChild>
@@ -61,8 +87,30 @@ const Navbar = () => {
             </TooltipContent>
           </Tooltip>
         ))}
-      </TooltipProvider>
-    </div>
+        <Separator orientation="vertical" className="h-10" />
+        {Object.values(MAPPING.socials).map((item, idx) => (
+          <Button key={idx} asChild variant={'ghost'} className={"grid place-items-center rounded-xl size-11 aspect-square"}>
+            <a href={item.link} target="_blank">
+              {React.createElement(item.icon, {
+                variant: 'outline',
+                className: 'size-5'
+              })}
+            </a>
+          </Button>
+        ))}
+        <Separator orientation="vertical" className="h-10" />
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant={'ghost'} className={"grid place-items-center rounded-xl size-11 aspect-square"}>
+              <LightIcon variant="outline" className="size-5" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent sideOffset={15}>
+            Light Mode
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </TooltipProvider>
   )
 };
 
