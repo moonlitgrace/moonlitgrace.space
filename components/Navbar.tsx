5e2fbadda9b14953889bd4f71c6df48aa261e8cd
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-20 20:21:11 +0530

    feat: add shadcn tooltip with button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,15 +5,35 @@ import HomeIcon from "@/components/icons/home";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { Button } from "@/components/ui/button";
+
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <div className="fixed left-1/2 transform -translate-x-1/2 bottom-10 border h-16 rounded-2xl p-2">
-      <Link href={'/'} className={cn(pathname === '/' && 'bg-primary', "grid place-items-center rounded-xl h-full aspect-square w-auto")}>
-        <HomeIcon variant={pathname === '/' ? 'solid' : 'outline'} className="size-5" />
-      </Link>
-    </div>
+    <TooltipProvider>
+      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-10 border h-16 rounded-2xl p-2">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button className={cn(pathname === '/' && 'bg-primary', "grid place-items-center rounded-xl size-full aspect-square")}>
+              <Link href={'/'}>
+                <HomeIcon variant={pathname === '/' ? 'solid' : 'outline'} className="size-5" />
+              </Link>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent sideOffset={15}>
+            <p>Home</p>
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </TooltipProvider>
   )
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,8 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
   ],
   prefix: "",
   theme: {
@@ -14,7 +12,7 @@ const config = {
       center: true,
       padding: "2rem",
       screens: {
-        "xl": "700px",
+        "xl": "650px",
       },
     },
     extend: {
@@ -52,6 +50,25 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       fontFamily: {
         sans: ["var(--font-sans)"],
