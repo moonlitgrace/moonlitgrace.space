c4c933de7c1613a613ae4e627257c2a434cbc36f
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-24 01:06:48 +0530

    refactor: rename utlitiy function

--- app/thoughts/[slug]/page.tsx
+++ app/thoughts/[slug]/page.tsx
@@ -2,7 +2,7 @@ import Markdown from "@/components/Markdown";
 import { Badge } from "@/components/ui/badge";
 import { db } from "@/db";
 import { posts, PostSelect } from "@/db/schema";
-import { formatTime } from "@/lib/utils";
+import { formatDate } from "@/lib/utils";
 import { eq } from "drizzle-orm";
 import removeMarkdown from "markdown-to-text";
 import { Metadata } from "next";
@@ -25,7 +25,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <>
       <div className="w-full flex flex-col items-center gap-5">
-        <h4 className="text-xs uppercase text-muted-foreground font-bold">{formatTime(postData.createdAt)}</h4>
+        <h4 className="text-xs uppercase text-muted-foreground font-bold">{formatDate(postData.createdAt)}</h4>
         <h1 className="text-5xl font-bold text-center leading-snug">{postData.title}</h1>
         <Badge className="capitalize">{postData.tag}</Badge>
       </div>
