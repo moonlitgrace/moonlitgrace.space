ce491e5588a6b2f3b73427b708e5aaef14a4fd06
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-24 00:34:12 +0530

    feat: retrieve posts from db on thougths page

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatTime(dateStr: Date) {
+  const date = new Date(dateStr).toLocaleDateString('en-EN', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  })
+  return date;
+}
