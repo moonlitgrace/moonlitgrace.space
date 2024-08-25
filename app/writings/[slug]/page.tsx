7fb35809835f58d8d5cae3e501175b460282d30f
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-25 23:47:11 +0530

    chore: test post cover in details page

--- app/writings/[slug]/page.tsx
+++ app/writings/[slug]/page.tsx
@@ -6,6 +6,7 @@ import { formatDate } from '@/lib/utils';
 import { eq } from 'drizzle-orm';
 import removeMarkdown from 'markdown-to-text';
 import { Metadata } from 'next';
+import Image from 'next/image';
 
 export async function generateMetadata({
   params,
@@ -39,6 +40,15 @@ export default async function Page({ params }: { params: { slug: string } }) {
         </h4>
         <h1 className="text-center text-4xl font-black leading-snug">{postData.title}</h1>
         <Badge className="capitalize">{postData.tag}</Badge>
+        <div className='relative w-full h-40'>
+          <Image
+            src={'https://res.cloudinary.com/dp9gvjh7m/image/upload/v1724599683/git-blog-header_twmtas.png'}
+            alt={postData.title}
+            fill
+            objectFit='cover'
+            className='rounded-2xl'
+          />
+        </div>
       </div>
       <Markdown markdown={postData.content} />
     </>
