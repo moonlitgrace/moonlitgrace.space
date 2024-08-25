995db18d6e4841691e3dc662cff71ad99ff31a0b
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-25 21:11:44 +0530

    feat: test add cloudinary image

--- app/writings/page.tsx
+++ app/writings/page.tsx
@@ -4,6 +4,7 @@ import { db } from '@/db';
 import { type PostSelect, posts } from '@/db/schema';
 import { formatDate } from '@/lib/utils';
 import { Metadata } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const metadata: Metadata = {
@@ -29,20 +30,33 @@ export default async function WritingsPage() {
       </h2>
       <div className="flex flex-col gap-5">
         {postsData.map((item) => (
-          <div key={item.id} className="flex flex-col">
-            <span className="text-xs font-bold uppercase text-muted-foreground">
-              {formatDate(item.createdAt)}
-            </span>
-            <div className="flex items-center gap-1 md:gap-2">
-              <Link
-                href={`/writings/${item.slug}`}
-                className="relative text-lg underline"
-              >
-                {item.title}
-              </Link>
-              <Separator className="flex flex-1" />
-              <Badge className="w-min capitalize">{item.tag}</Badge>
+          <div className='flex flex-col gap-3'>
+            <div key={item.id} className="flex flex-col">
+              <span className="text-xs font-bold uppercase text-muted-foreground">
+                {formatDate(item.createdAt)}
+              </span>
+              <div className="flex items-center gap-1 md:gap-2">
+                <Link
+                  href={`/writings/${item.slug}`}
+                  className="relative text-lg underline"
+                >
+                  {item.title}
+                </Link>
+                <Separator className="flex flex-1" />
+                <Badge className="w-min capitalize">{item.tag}</Badge>
+              </div>
             </div>
+            {item.id === 19 && (
+              <div className='relative w-full h-40'>
+                <Image
+                  src={'https://res.cloudinary.com/dp9gvjh7m/image/upload/v1724599683/git-blog-header_twmtas.png'}
+                  alt={item.title}
+                  fill
+                  objectFit='cover'
+                  className='rounded-2xl'
+                />
+              </div>
+            )}
           </div>
         ))}
       </div>
