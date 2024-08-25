bbc55f9aefbbd4dc8556cd9d56d1ed9bab3ada91
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-25 20:07:57 +0530

    chore: remove crafts page and assets

--- app/crafts/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function CraftsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <main className="mb-40 mt-10 flex w-full flex-col gap-10">{children}</main>;
-}
-
