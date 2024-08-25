1660387554b49e1ea6d280295049d66346dc3c15
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-26 00:36:38 +0530

    fix: next.js bootup compile time error with config.ts

--- hooks/useScreenDetector.ts
+++ hooks/useScreenDetector.ts
@@ -15,7 +15,7 @@ export const useScreenDetector = () => {
     }
   }, [])
 
-  const isMobile = width <= 768;
+  const isMobile = width < 768;
   const isTablet = width <= 1024;
   const isDesktop = width > 1024;
 
