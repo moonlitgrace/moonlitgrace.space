d1f75069f2da0b5601bf6bdd4f54ef82db3ba4d4
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-25 14:23:24 +0530

    feat: add basic crafts page

--- app/crafts/layout.tsx
+++ app/crafts/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
-export default function WritingsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function CraftsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <main className="mb-40 mt-10 flex w-full flex-col gap-10">{children}</main>;
 }
+
