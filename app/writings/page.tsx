9f375e0684b356915859f3692926fcd91bab87ab
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-25 20:55:03 +0530

    feat: add custom  tailwind utility layer

--- app/writings/page.tsx
+++ app/writings/page.tsx
@@ -7,7 +7,7 @@ import { Metadata } from 'next';
 import Link from 'next/link';
 
 export const metadata: Metadata = {
-  title: 'Thoughts. | moonlitspace',
+  title: 'Writings. | moonlitspace',
   description: 'my graceful thoughts',
 };
 
@@ -33,14 +33,14 @@ export default async function WritingsPage() {
             <span className="text-xs font-bold uppercase text-muted-foreground">
               {formatDate(item.createdAt)}
             </span>
-            <div className="flex flex-col gap-1 md:flex-row md:items-center md:gap-2">
+            <div className="flex items-center gap-1 md:gap-2">
               <Link
                 href={`/writings/${item.slug}`}
-                className="relative text-lg before:absolute before:bottom-1 before:h-0.5 before:w-full before:bg-transparent before:duration-200 hover:before:bottom-0 hover:before:bg-primary"
+                className="relative text-lg underline"
               >
                 {item.title}
               </Link>
-              <Separator className="hidden md:flex md:flex-1" />
+              <Separator className="flex flex-1" />
               <Badge className="w-min capitalize">{item.tag}</Badge>
             </div>
           </div>
