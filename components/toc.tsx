e61ffe42d161c46a03dc66b95dbfafaffd6ca571
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-26 18:28:22 +0530

    fix: toc open state dynamic render

--- components/toc.tsx
+++ components/toc.tsx
@@ -1,28 +1,34 @@
-'use client'
+'use client';
 
 import { escapeText } from '@/lib/utils';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Button } from './ui/button';
 import ListIcon from './icons/list';
+import { useState } from 'react';
 
 const TableOfContents = ({ headings }: { headings: string[] }) => {
+  const [tocOpen, setTocOpen] = useState(false);
+
   return (
-    <div className='fixed right-4 top-1/2 hidden md:flex -translate-y-1/2 border bg-background p-2 rounded-2xl'>
-      <Popover onOpenChange={(open) => console.log(open)}>
+    <div className="fixed right-4 top-1/2 hidden -translate-y-1/2 rounded-2xl border bg-background p-2 md:flex">
+      <Popover onOpenChange={setTocOpen}>
         <PopoverTrigger asChild>
-          <Button variant='ghost' className={'grid aspect-square size-11 place-items-center rounded-xl'}>
-            <ListIcon className='size-5' variant='outline' />
+          <Button
+            variant="ghost"
+            className={'grid aspect-square size-11 place-items-center rounded-xl'}
+          >
+            <ListIcon className="size-5" variant={tocOpen ? 'solid' : 'outline'} />
           </Button>
         </PopoverTrigger>
-        <PopoverContent sideOffset={20} side='left' className="flex-col bg-background gap-4 rounded-2xl border p-5 w-max md:flex">
+        <PopoverContent
+          sideOffset={20}
+          side="left"
+          className="w-max flex-col gap-4 rounded-2xl border bg-background p-5 md:flex"
+        >
           <h3 className="font-bold">Table of Contents</h3>
           <div className="flex flex-col gap-1">
             {headings.map((heading) => (
-              <a className="font-medium underline w-max" href={`#${escapeText(heading)}`}>
+              <a className="w-max font-medium underline" href={`#${escapeText(heading)}`}>
                 <span className="text-primary">#.&nbsp;</span>
                 {heading}
               </a>
