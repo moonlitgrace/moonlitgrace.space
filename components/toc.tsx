63da27f63298f42a202b7d12252119d2c49f7c4b
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-26 12:01:16 +0530

    fix: TOC layouts and custom tailwind utlity class

--- components/toc.tsx
+++ components/toc.tsx
@@ -1,18 +1,19 @@
-import { escapeText } from "@/lib/utils";
+import { escapeText } from '@/lib/utils';
 
 const TableOfContents = ({ headings }: { headings: string[] }) => {
   return (
-    <div className="fixed right-10 top-1/2 -translate-y-1/2 flex flex-col gap-4 p-5 border rounded-2xl">
+    <div className="fixed right-10 top-1/2 hidden -translate-y-1/2 flex-col gap-4 rounded-2xl border p-5 md:flex">
       <h3 className="font-bold">Table of Contents</h3>
       <div className="flex flex-col gap-1">
         {headings.map((heading) => (
           <a className="font-medium underline" href={`#${escapeText(heading)}`}>
-            <span className="text-primary">#.&nbsp;</span>{heading}
+            <span className="text-primary">#.&nbsp;</span>
+            {heading}
           </a>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TableOfContents;
