a4a84d13f9f6599eedd95eef192f23400c6b6c10
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-28 10:32:09 +0530

    feat: add admin blog crud actions

---
+++ app/api/blog/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/db";
+import { posts } from "@/db/schema";
+import { AdminBlogData } from "@/zod_schemas/admin";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import slugify from "slugify";
+
+export async function PATCH(request: NextRequest) {
+  const data: AdminBlogData = await request.json();
+  const isToUpdate = data.id !== undefined;
+
+  try {
+    if (isToUpdate) {
+      await db.update(posts)
+        .set({
+          title: data.title,
+          tag: data.tag,
+          content: data.content,
+          slug: slugify(data.title.toLowerCase()),
+        })
+        .where(eq(posts.id, data.id as number))
+
+      return NextResponse.json({ message: 'Success' });
+    } else {
+      await db.insert(posts).values({
+        title: data.title,
+        tag: data.tag,
+        content: data.content,
+        slug: slugify(data.title.toLowerCase()),
+      })
+
+      return NextResponse.json({ message: 'Success' });
+    }
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ message: 'Data update failed' }, { status: 500 })
+  }
+}
