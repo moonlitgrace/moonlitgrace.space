5f9ccfcd1b09c39a74bf4e2bbeacc8d930dc6c15
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-28 18:28:50 +0530

    feat: add admin index route tables

---
+++ app/api/admin/route.ts
@@ -0,0 +1,19 @@
+import { db } from '@/db';
+import { posts } from '@/db/schema';
+import { count } from 'drizzle-orm';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const postsCount = (await db.select({ count: count() }).from(posts))[0];
+
+    const obj = {
+      posts: postsCount,
+    };
+
+    return NextResponse.json(obj);
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: 'Data counting failed!' }, { status: 500 });
+  }
+}
