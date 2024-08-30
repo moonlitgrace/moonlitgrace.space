a187c49c44640edb0a913f6dd8eb2c03a9f5dccb
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-30 21:59:38 +0530

    feat: add nextjs env types

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-export const connection = postgres(process.env.DATABASE_URL as string);
+export const connection = postgres(process.env.DATABASE_URL);
 export const db = drizzle(connection);
