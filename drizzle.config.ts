3f2d5cd5f89310713c7984a0a0852df34e2ad380
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-30 21:59:38 +0530

    feat: add nextjs env types

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,12 @@
 import './env-config';
 import { defineConfig } from 'drizzle-kit';
 
-console.log(process.env.DATABASE_URL);
-
 export default defineConfig({
   dialect: 'postgresql',
   schema: './db/schema.ts',
   out: './db/migrations',
   dbCredentials: {
-    url: process.env.DATABASE_URL as string,
+    url: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
---
+++ environment.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL: string;
+    SECRET_KEY: string;
+    ADMIN_USERNAME: string;
+    ADMIN_PASSWORD: string;
+  }
+}
+
