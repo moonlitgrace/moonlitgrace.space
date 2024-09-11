5abe6d0669ec25a03df596391da5342157fa6fa8
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-11 15:20:00 +0530

    fix: admin blog page table and add dialog

--- app/_components/_admin/BlogDeleteButton.tsx
+++ app/_components/_admin/BlogDeleteButton.tsx
@@ -3,6 +3,18 @@
 import { Button } from '@/components/ui/button';
 import { revalidatePathClient } from '@/helpers/revalidate';
 import { useState } from 'react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import TrashIcon from '@/components/icons/trash';
 
 export default function AdminBlogDeleteButton({ postId }: { postId: number }) {
   const [pending, setPending] = useState(false);
@@ -20,7 +32,7 @@ export default function AdminBlogDeleteButton({ postId }: { postId: number }) {
           },
         });
 
-        revalidatePathClient('/admin/blog');
+        revalidatePathClient();
       }
     } catch (err) {
       console.error(err);
@@ -30,8 +42,25 @@ export default function AdminBlogDeleteButton({ postId }: { postId: number }) {
   }
 
   return (
-    <Button disabled={pending} variant="destructive" size="sm" onClick={handleDeleteBlog}>
-      Delete
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button size="icon" className="size-8" variant="destructive">
+          <TrashIcon variant="outline" className="size-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete post (id={postId}) from
+            record.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
