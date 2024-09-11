de1cf76b3dc0ac168f96e9076f65f702c712302d
Moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-11 17:02:38 +0530

    fix: admin blog page (#51)

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -10,9 +10,9 @@ export default function ProtectedLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="mb-40 mt-10 flex w-full flex-col gap-10 md:mb-10">
+    <div className="mb-40 mt-10 flex w-full flex-col gap-10 md:mb-10">
       <DynamicAdminBar />
       {children}
-    </main>
+    </div>
   );
 }
---
+++ components/icons/plus.tsx
@@ -0,0 +1,16 @@
+import { IconProps } from '@/interfaces/icon';
+
+export function PlusIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>
+  );
+}
