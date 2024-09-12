fe477b056b9208fadf00af0a9720199818bceb47
Danish <102005928+daaniissh@users.noreply.github.com>
2024-09-12 19:39:37 +0600

    feat: cloudinary support for uploads (#48)
    Co-authored-by: moonlitgrace <moonlitgrace.gaia@gmail.com>

--- actions/admin.ts
+++ actions/admin.ts
@@ -1,8 +1,7 @@
+import { validateFile } from '@/lib/utils';
 import { AdminBlogFormState, AdminBlogSchema } from '@/zod_schemas/admin';
 
 export default async function adminBlogSubmit(state: AdminBlogFormState, formData: FormData) {
-  console.log(state);
-
   const validatedFields = AdminBlogSchema.safeParse({
     id: formData.get('id') ? Number(formData.get('id')) : undefined,
     title: formData.get('title'),
@@ -18,6 +17,27 @@ export default async function adminBlogSubmit(state: AdminBlogFormState, formDat
   }
 
   try {
+    let coverImage = formData.get('cover') as File;
+    // coverImage returns invalid File object if nothing is selected
+    if (validateFile(coverImage)) {
+      const imageData = new FormData();
+      imageData.append('file', coverImage);
+
+      const imgRes = await fetch('/api/cloudinary', {
+        method: 'POST',
+        body: imageData,
+      });
+
+      if (!imgRes.ok) {
+        const imgResErr = await imgRes.json();
+        console.error('Image upload failed:', imgResErr);
+        return { message: imgResErr };
+      }
+
+      const data = await imgRes.json();
+      validatedFields.data.cover = data.url;
+    }
+
     const res = await fetch('/api/blog', {
       method: 'POST',
       body: JSON.stringify(validatedFields.data),
