21e34fd0b2d42a784341da0bbc57c14ed4cf052f
Moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-13 10:16:32 +0530

    revert: "refactor: move drizzle migrations outside" (#61)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'postgresql',
   schema: './db/schema.ts',
-  out: './.drizzle',
+  out: './db/migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
