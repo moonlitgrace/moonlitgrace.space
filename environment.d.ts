d24c118ec10486f84b1dd85b3497ed61bbf89994
Moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-14 08:28:53 +0530

    fix(cloud): run sentry only on production env (#68)

--- environment.d.ts
+++ environment.d.ts
@@ -1,13 +1,18 @@
 /* eslint-disable no-unused-vars */
 namespace NodeJS {
   interface ProcessEnv {
+    // DATABASE
     DATABASE_URL: string;
+    // SECRETS
     SECRET_KEY: string;
+    // ADMIN CREDS
     ADMIN_USERNAME: string;
     ADMIN_PASSWORD: string;
+    // CLOUDINARY
     CLOUDINARY_CLOUD_NAME: string;
     CLOUDINARY_API_KEY: string;
     CLOUDINARY_API_SECRET: string;
-    NEXT_PUBLIC_SENTRY_DSN: string;
+    // SENTRY CREDS
+    SENTRY_DSN: string;
   }
 }
