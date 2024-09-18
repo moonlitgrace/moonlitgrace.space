d790fedbdb521cd9b11d3541360d8f84dba64152
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-18 12:20:04 +0530

    fix: lints

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import { arrayBufferToBase64 } from '@/lib/utils';
 import { ImageResponse } from 'next/og';
 
