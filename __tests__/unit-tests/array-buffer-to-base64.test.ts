04b8fc5d03c4cc98d765d3cfa0173d306e8ba851
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-18 15:59:01 +0530

    test: add test and refactor structure

---
+++ __tests__/unit-tests/array-buffer-to-base64.test.ts
@@ -0,0 +1,10 @@
+import { arrayBufferToBase64 } from "@/lib/utils";
+
+describe('array buffer to base64', () => {
+  it('should convert ArrayBuffer to base64 string', () => {
+    // input: ArrayBuffer from a Uint8Array [72, 101, 108, 108, 111] (which represents "Hello")
+    const buffer = new Uint8Array([72, 101, 108, 108, 111]).buffer;
+    expect(arrayBufferToBase64(buffer)).toBe('SGVsbG8=');
+  });
+});
+
