f02b618d5c94b826a62acb4f492defb3a1645b2e
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-20 22:50:29 +0530

    feat: 404 page

---
+++ app/(routes)/(main)/blog/error.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import IsometricHandler from "@/components/isometric-handler"
+import Svg404 from '@/assets/svgs/404.svg'
+import { usePathname } from "next/navigation"
+import { Button } from "@/components/ui/button"
+import PencilIcon from "@/components/icons/pencil"
+import Link from "next/link"
+
+export default function Error() {
+  const pathname = usePathname()
+
+  return (
+    <div className="size-full flex flex-col gap-5 justify-center">
+      <IsometricHandler svg={<Svg404 />} />
+      <div className="flex flex-col gap-2">
+        <h2 className="text-3xl font-bold">
+          Page not found
+          <span className="text-primary">.</span>
+        </h2>
+        <p className="text-muted-foreground">path: {pathname}</p>
+        <Link href="/blog">
+          <Button className="w-max gap-2">See all Posts <PencilIcon variant="solid" className="size-3" /></Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
